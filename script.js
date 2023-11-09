2a58fee05b4870dfcd5a74fd836ff7c1bf3474bd
KAPIL BISHNOI <149925526+KapilBishnoi90000@users.noreply.github.com>
2023-11-09 16:49:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var a;
+function pass(){
+    if(a==1)
+
+    {
+        document.getElementById('password').type='password';
+        document.getElementById('pass-icon').src="https://icons.veryicon.com/png/o/miscellaneous/hekr/action-hide-password.png";
+        a=0;
+    }
+
+    else
+    {
+        document.getElementById('password').type='text';
+        document.getElementById('pass-icon').src="https://i.pinimg.com/originals/67/d4/4a/67d44a56f6c480f2775016ac7cd035c5.png";
+        a=1;
+    }
+
+    
+}
+function auth(){
+				
+    var email = document.getElementById("email").value;
+    
+    var password = document.getElementById("password").value;
+     if(email=="kapilbishnoi@90000" && password=="kapilbishnoi@3170582")
+     {  window.location.assign ("https://kapilbishnoi90000.github.io/KAPIL/Admin-page.html");
+     alert("logged in successfully.click ok for next");
+    }
+    else{
+        alert("Invalid ID or Password");
+        return;
+    }
+}
